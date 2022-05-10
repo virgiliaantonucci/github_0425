928f44fc3d864bc39b4a86962c8b885c67dff846
Patrick Bialy <patrickbialy@yahoo.com>
2022-05-10 09:29:08 -0400

    Updated

--- index.js
+++ index.js
@@ -1,55 +1,84 @@
-/* //testing the API
+const breweryDetails = document.querySelector('.brewery-details');
+const breweryList = document.getElementById('brewery-list');
+const breweryName = document.getElementById('brewery-name');
+const breweryImgine = document.getElementById('brewery-image');
+const breweryDescription = document.getElementById('brewery_type');
+const descriptionForm = document.getElementById('description-form');
+const reviewForm = document.getElementById('review-form');
+const reviewList = document.getElementById('review-list');
 
-// your code here
-const baseUrl = 'https://api.openbrewerydb.org/breweries'
 
-/* const cakeDetails = document.querySelector('.cake-details')
-
-const cakeList = document.getElementById('cake-list')
-const cakeName = document.getElementById('cake-name')
-const cakeImage = document.getElementById('cake-image')
-const cakeDescription = document.getElementById('cake-description')
-
-const descriptionForm = document.getElementById('description-form')
-const reviewForm = document.getElementById('review-form')
-const reviewList = document.getElementById('review-list')
-
-// fetch(`${baseUrl}/cakes/1`)
-//   .then(res => res.json())
-//   .then(cake => renderCake(cake))
-
-// fetch(`${baseUrl}/cakes`)
-//   .then(res => res.json())
-//   .then(cakes => {
-//     renderMenu(cakes)
-// })
-*/
-
-/* function getCakes() {
-fetch(`${baseUrl}`)
+fetch('https://api.sampleapis.com/beers/stouts')
     .then(resp => resp.json())
-    .then(cakes => console.log(cakes))
-}
+     .then(brewery => {
+         renderAllBreweries(brewery);
+     });
 
-getCakes() */
+     function renderAllBreweries(brewery) {
+         breweryList.innerHTML = '';
+         brewery.forEach(brewery => {
+             const li = document.createElement('li');
+             li.innerText = brewery.name;
+             breweryList.appendChild(li);
+         })
+     }
 
 
-/* let request = new Request('https://api.openbrewerydb.org/breweries', {
-    headers: new Headers({
-        'Content-Type': 'text/json' 
-    }),
-    method: 'GET'
-}); */
 
-/* fetch('https://api.openbrewerydb.org/breweries').then((response) => {
-    resp => resp.json()
-    console.log(response);
-    return response.text;
+
+
+
+
+// fetch(`https://api.openbrewerydb.org/breweries`)
+//      .then(resp => resp.json())
+//      .then(brewery => {
+//          renderOneBrewery(brewery[2]);
+//      })
+
+//      function renderOneBrewery(brewery) {
+//         breweryName.innerText = brewery.name;
+
+
+// }
+//  fetch(`https://api.openbrewerydb.org/breweries`)
+//      .then(resp => resp.json())
+//      .then(breweries =>  {
+//          renderBreweries(breweries);
+//          console.log(breweries);
+//      });
+
+//      function renderBreweries(brewery) {
+//          breweryList.innerHTML = '';
+//          brewery.forEach(brewery => {
+//              const li = document.createElement('li');
+//              li.innerText = brewery.name;
+//              breweryList.appendChild(li);
+
+            
+                
+            
+//          })    
+//  };
+//      reviewForm.addEventListener('submit', (e) => handleReviewForm(e));
+
+//      function handleReviewForm(e) {
+//          e.preventDefault();
+
+//          const li = document.createElement('li');
+//          li.innerText = reviewForm.review.value;
+//          reviewList.appendChild(li);
+
+//          reviewForm.reset();
+//      }
     
-}); */
+//      descriptionForm.addEventListener('submit', (e) => handleDescriptionForm(e));
 
-fetch(`https://api.openbrewerydb.org/breweries`)
-    .then(resp => resp.json())
-    .then(breweries => console.log(breweries))
+//      function handleDescriptionForm(e) {
+//          e.preventDefault();
+//          const li = document.createElement('li');
+//          li.innerText = descriptionForm.description.value;
+//          breweryDescription.appendChild(li);
 
-    
+//          descriptionForm.reset();
+//      }
+     
