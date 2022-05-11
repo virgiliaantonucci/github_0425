88fced4dc5bb251403cf07ed08de96d89ba1c56b
Patrick Bialy <patrickbialy@yahoo.com>
2022-05-11 14:39:36 -0400

    beerkeeper

--- index.js
+++ index.js
@@ -1,84 +1,62 @@
-const breweryDetails = document.querySelector('.brewery-details');
-const breweryList = document.getElementById('brewery-list');
-const breweryName = document.getElementById('brewery-name');
-const breweryImgine = document.getElementById('brewery-image');
-const breweryDescription = document.getElementById('brewery_type');
-const descriptionForm = document.getElementById('description-form');
+const beerDetails = document.querySelector('.beer-details');
+const beerList = document.getElementById('beer-list');
+const beerName = document.getElementById('beer-name');
+const beerImgine = document.getElementById('beer-image');
 const reviewForm = document.getElementById('review-form');
-const reviewList = document.getElementById('review-list');
+const beerPrice = document.getElementById('beer-price');
+const weeklyUpdate = document.getElementById('weekly-updates');
 
 
 fetch('https://api.sampleapis.com/beers/stouts')
     .then(resp => resp.json())
-     .then(brewery => {
-         renderAllBreweries(brewery);
-     });
+     .then(beers => {
+         renderBeers(beers);
+         renderOneBeer(beers[21]);
+             console.log(beers);
+     })
+            // rendering beers in the beer-list column 
+        function renderBeers(beers) {
+            beerList.innerHTML = '';
+            beers.forEach(beer => {
+                const li = document.createElement('li');
+                li.innerText = beer.name;
+                beerList.appendChild(li);
+                    // click event added for each beer + img + price
+                li.addEventListener('click', () => {
+                    beerName.innerText = beer.name;
+                    beerImgine.src = beer.image;
+                    beerPrice.innerText = beer.price;
+                })
+    })
+        }
+            //trying to get the a submit event to happen for the email subcription form
+        weeklyUpdate.addEventListener('submit', (e) =>  handleFormSubmit(e));
 
-     function renderAllBreweries(brewery) {
-         breweryList.innerHTML = '';
-         brewery.forEach(brewery => {
+        function handleFormSubmit(e) {
+            e.preventDefault();
+
+            const li = document.createElement('li');   
+            li.innerText = weeklyUpdate.value;
+
+             reviewList.appendChild(li);
+
+            weeklyUpdate.reset();
+            
+        }
+            // just rendering one beer(img) when the page loads (the nitro can)
+        function renderOneBeer(beer) {
+            beerImgine.src = beer.image;
+        }
+
+            // trying to add a review submit event to the review form, so far its cereating a list under the add review button
+         reviewForm.addEventListener('submit', (e) => handleReviewSubmit(e));
+
+         function handleReviewSubmit(e) {
+             e.preventDefault();
+
              const li = document.createElement('li');
-             li.innerText = brewery.name;
-             breweryList.appendChild(li);
-         })
-     }
+             li.innerText = e.target.review.value;
+             reviewForm.appendChild(li);
 
-
-
-
-
-
-
-// fetch(`https://api.openbrewerydb.org/breweries`)
-//      .then(resp => resp.json())
-//      .then(brewery => {
-//          renderOneBrewery(brewery[2]);
-//      })
-
-//      function renderOneBrewery(brewery) {
-//         breweryName.innerText = brewery.name;
-
-
-// }
-//  fetch(`https://api.openbrewerydb.org/breweries`)
-//      .then(resp => resp.json())
-//      .then(breweries =>  {
-//          renderBreweries(breweries);
-//          console.log(breweries);
-//      });
-
-//      function renderBreweries(brewery) {
-//          breweryList.innerHTML = '';
-//          brewery.forEach(brewery => {
-//              const li = document.createElement('li');
-//              li.innerText = brewery.name;
-//              breweryList.appendChild(li);
-
-            
-                
-            
-//          })    
-//  };
-//      reviewForm.addEventListener('submit', (e) => handleReviewForm(e));
-
-//      function handleReviewForm(e) {
-//          e.preventDefault();
-
-//          const li = document.createElement('li');
-//          li.innerText = reviewForm.review.value;
-//          reviewList.appendChild(li);
-
-//          reviewForm.reset();
-//      }
-    
-//      descriptionForm.addEventListener('submit', (e) => handleDescriptionForm(e));
-
-//      function handleDescriptionForm(e) {
-//          e.preventDefault();
-//          const li = document.createElement('li');
-//          li.innerText = descriptionForm.description.value;
-//          breweryDescription.appendChild(li);
-
-//          descriptionForm.reset();
-//      }
-     
+             reviewForm.reset();
+         }
